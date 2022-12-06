ff14a0a2345331753ef5a6af6f808082beb1e582
rducrot <ducrot.romain@gmail.com>
2022-12-06 16:30:18 +0100

    Filtres

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from shop.models import Category, Product
+from shop.models import Category, Product, Article
 
 
 class CategorySerializer(ModelSerializer):
@@ -13,3 +13,9 @@ class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'date_created', 'date_updated', 'name', 'category']
+
+
+class ArticleSerialize(ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id', 'date_created', 'date_updated', 'name', 'price', 'product']
--- shop/views.py
+++ shop/views.py
@@ -1,21 +1,33 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from shop.models import Category, Product
-from shop.serializers import CategorySerializer, ProductSerializer
+from shop.models import Category, Product, Article
+from shop.serializers import CategorySerializer, ProductSerializer, ArticleSerialize
 
 
 class CategoryViewSet(ReadOnlyModelViewSet):
-
     serializer_class = CategorySerializer
 
     def get_queryset(self):
-        return Category.objects.all()
+        return Category.objects.filter(active=True)
 
 
 class ProductViewSet(ReadOnlyModelViewSet):
-
     serializer_class = ProductSerializer
 
     def get_queryset(self):
-        return Product.objects.all()
+        queryset = Product.objects.filter(active=True)
+        category_id = self.request.GET.get('category_id')
+        if category_id is not None:
+            queryset = queryset.filter(category_id=category_id)
+        return queryset
 
+
+class ArticleViewSet(ReadOnlyModelViewSet):
+    serializer_class = ArticleSerialize
+
+    def get_queryset(self):
+        queryset = Article.objects.filter(active=True)
+        product_id = self.request.GET.get('product_id')
+        if product_id is not None:
+            queryset = queryset.filter(product_id=product_id)
+        return queryset
