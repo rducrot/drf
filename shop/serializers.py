ff4cc3550eb7e48875410218482473e38f1349b0
rducrot <ducrot.romain@gmail.com>
2022-12-12 11:52:25 +0100

    Tests

--- shop/serializers.py
+++ shop/serializers.py
@@ -4,18 +4,21 @@ from shop.models import Category, Product, Article
 
 
 class CategorySerializer(ModelSerializer):
+
     class Meta:
         model = Category
-        fields = ['id', 'name']
+        fields = ['id', 'date_created', 'date_updated', 'name']
 
 
 class ProductSerializer(ModelSerializer):
+
     class Meta:
         model = Product
         fields = ['id', 'date_created', 'date_updated', 'name', 'category']
 
 
-class ArticleSerialize(ModelSerializer):
+class ArticleSerializer(ModelSerializer):
+
     class Meta:
         model = Article
         fields = ['id', 'date_created', 'date_updated', 'name', 'price', 'product']
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,11 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from shop.models import Category, Product, Article
-from shop.serializers import CategorySerializer, ProductSerializer, ArticleSerialize
+from shop.serializers import CategorySerializer, ProductSerializer, ArticleSerializer
 
 
 class CategoryViewSet(ReadOnlyModelViewSet):
+
     serializer_class = CategorySerializer
 
     def get_queryset(self):
@@ -12,18 +13,20 @@ class CategoryViewSet(ReadOnlyModelViewSet):
 
 
 class ProductViewSet(ReadOnlyModelViewSet):
+
     serializer_class = ProductSerializer
 
     def get_queryset(self):
         queryset = Product.objects.filter(active=True)
         category_id = self.request.GET.get('category_id')
-        if category_id is not None:
+        if category_id:
             queryset = queryset.filter(category_id=category_id)
         return queryset
 
 
 class ArticleViewSet(ReadOnlyModelViewSet):
-    serializer_class = ArticleSerialize
+
+    serializer_class = ArticleSerializer
 
     def get_queryset(self):
         queryset = Article.objects.filter(active=True)
