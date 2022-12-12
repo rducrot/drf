b34732798638076ee4d726c106f00f3ed3562c32
rducrot <ducrot.romain@gmail.com>
2022-12-12 15:51:47 +0100

    Serializers liste et détail

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,25 +1,25 @@
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from rest_framework import serializers
 
 from shop.models import Category, Product, Article
 
 
-class CategorySerializer(ModelSerializer):
-
-    products = SerializerMethodField()
+class ArticleSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Category
-        fields = ['id', 'date_created', 'date_updated', 'name', 'products']
-
-    def get_products(self, instance):
-        queryset = instance.products.filter(active=True)
-        serializer = ProductSerializer(queryset, many=True)
-        return serializer.data
+        model = Article
+        fields = ['id', 'date_created', 'date_updated', 'name', 'price', 'product']
 
 
-class ProductSerializer(ModelSerializer):
+class ProductListSerializer(serializers.ModelSerializer):
 
-    articles = SerializerMethodField()
+    class Meta:
+        model = Product
+        fields = ['id', 'date_created', 'date_updated', 'name', 'category']
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+
+    articles = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
@@ -31,8 +31,22 @@ class ProductSerializer(ModelSerializer):
         return serializer.data
 
 
-class ArticleSerializer(ModelSerializer):
+class CategoryListSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Article
-        fields = ['id', 'date_created', 'date_updated', 'name', 'price', 'product']
+        model = Category
+        fields = ['id', 'date_created', 'date_updated', 'name']
+
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+
+    products = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = ['id', 'date_created', 'date_updated', 'name', 'products']
+
+    def get_products(self, instance):
+        queryset = instance.products.filter(active=True)
+        serializer = ProductListSerializer(queryset, many=True)
+        return serializer.data
--- shop/views.py
+++ shop/views.py
@@ -1,20 +1,28 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from shop.models import Category, Product, Article
-from shop.serializers import CategorySerializer, ProductSerializer, ArticleSerializer
+from shop.serializers import CategoryListSerializer, CategoryDetailSerializer,\
+    ProductListSerializer, ProductDetailSerializer, ArticleSerializer
 
 
 class CategoryViewSet(ReadOnlyModelViewSet):
 
-    serializer_class = CategorySerializer
+    serializer_class = CategoryListSerializer
+    detail_serializer_class = CategoryDetailSerializer
 
     def get_queryset(self):
         return Category.objects.filter(active=True)
 
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return self.detail_serializer_class
+        return super().get_serializer_class()
+
 
 class ProductViewSet(ReadOnlyModelViewSet):
 
-    serializer_class = ProductSerializer
+    serializer_class = ProductListSerializer
+    detail_serializer_class = ProductDetailSerializer
 
     def get_queryset(self):
         queryset = Product.objects.filter(active=True)
@@ -22,6 +30,11 @@ class ProductViewSet(ReadOnlyModelViewSet):
         if category_id:
             queryset = queryset.filter(category_id=category_id)
         return queryset
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return self.detail_serializer_class
+        return super().get_serializer_class()
 
 
 class ArticleViewSet(ReadOnlyModelViewSet):
