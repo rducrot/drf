d692886c7003ffaa2de076ade02d109c2a524947
rducrot <ducrot.romain@gmail.com>
2022-12-12 14:06:44 +0100

    Détails serializer

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,20 +1,34 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from shop.models import Category, Product, Article
 
 
 class CategorySerializer(ModelSerializer):
 
+    products = SerializerMethodField()
+
     class Meta:
         model = Category
-        fields = ['id', 'date_created', 'date_updated', 'name']
+        fields = ['id', 'date_created', 'date_updated', 'name', 'products']
+
+    def get_products(self, instance):
+        queryset = instance.products.filter(active=True)
+        serializer = ProductSerializer(queryset, many=True)
+        return serializer.data
 
 
 class ProductSerializer(ModelSerializer):
 
+    articles = SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = ['id', 'date_created', 'date_updated', 'name', 'category']
+        fields = ['id', 'date_created', 'date_updated', 'name', 'category', 'articles']
+
+    def get_articles(self, instance):
+        queryset = instance.articles.filter(active=True)
+        serializer = ArticleSerializer(queryset, many=True)
+        return serializer.data
 
 
 class ArticleSerializer(ModelSerializer):
